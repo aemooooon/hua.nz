d18d070191feb5d60ee0303e178a5a411fed370a
Hua Wang <hwa205@uclive.ac.nz>
2025-01-06 15:35:39 +1300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { EffectLorenzAttractor } from "./components/EffectLorenzAttractor";
 import EffectPixelDistortion from "./components/EffectPixelDistortion";
 import EffectHeartBeats from "./components/EffectHeartBeats";
 import hua from "./assets/images/hua.jpeg";
-import "./styles/App.css";
 
 const App = () => {
     const [currentEffect, setCurrentEffect] = useState("effectfuse");
@@ -22,6 +21,7 @@ const App = () => {
             newCanvas.style.left = "0";
             newCanvas.style.width = "100%";
             newCanvas.style.height = "100%";
+            newCanvas.style.zIndex = "-1";
             document.body.appendChild(newCanvas);
             return newCanvas;
         };
@@ -45,8 +45,13 @@ const App = () => {
             });
         }
 
-        const threeColors = [new THREE.Color(0x032820), new THREE.Color(0x80a416), new THREE.Color(0xad9f3c)];
-
+        const threeColors = [
+            new THREE.Color(0x1d2012), // 深绿色
+            new THREE.Color(0xafcc8f), // 浅绿色
+            new THREE.Color(0x7ca65c), // 中绿色
+            new THREE.Color(0x5d7d4b), // 次中绿色
+            new THREE.Color(0x768e90), // 蓝绿色
+        ];
         const rgbColors = convertColorsToRGBFormat(threeColors);
 
         if (currentEffect === "effectfuse") {
@@ -71,7 +76,7 @@ const App = () => {
         } else if (currentEffect === "effectlorenzattractor") {
             canvas = createCanvas();
             const params = {
-                fireballColor: new THREE.Color(0xff4500),
+                fireballColor: new THREE.Color(0xafcc8f),
                 particleColors: threeColors,
             };
             effectInstance = new EffectLorenzAttractor(canvas, params);
@@ -81,7 +86,7 @@ const App = () => {
             const params = {
                 width: 180,
                 height: 130,
-                color: "#FF4500", 
+                color: "#7CA65C",
                 particles: {
                     length: 600,
                     duration: 2,
@@ -111,57 +116,66 @@ const App = () => {
 
     return (
         <>
-            <header className="animated">
-                <a href="/" className="logo audiowide-regular" onClick={() => setCurrentEffect("bgshader01")}>
+            <header className="fixed top-0 left-0 w-full text-white z-50 bg-primary-dark text-light p-8 flex justify-between items-center">
+                <span className="text-4xl font-bold font-audiowide" onClick={() => setCurrentEffect("effectfuse")}>
                     Portfolio.
-                </a>
-                <nav className="navbar">
-                    <a href="" className="active" onClick={() => setCurrentEffect("effectfuse")}>
+                </span>
+                <nav className="flex space-x-4">
+                    <a href="#" className="hover-effect" onClick={() => setCurrentEffect("effectfuse")}>
                         Home
                     </a>
-                    <a href="#" onClick={() => setCurrentEffect("effectlorenzattractor")}>
+                    <a href="#" className="hover-effect" onClick={() => setCurrentEffect("effectlorenzattractor")}>
                         About
                     </a>
-                    <a href="#" onClick={() => setCurrentEffect("effectmonjori")}>
+                    <a href="#" className="hover-effect" onClick={() => setCurrentEffect("effectmonjori")}>
                         Project
                     </a>
-                    <a href="#" onClick={() => setCurrentEffect("heartEffect")}>
+                    <a href="#" className="hover-effect" onClick={() => setCurrentEffect("heartEffect")}>
                         Contact
                     </a>
                 </nav>
             </header>
 
-            <section className="home animated">
-                <div className="home-detail">
-                    <h1 className="beau-rivage-regular mb-6">Hua Wang</h1>
-                    <h2>Web Developer / Data Engineer</h2>
-                    <p>
+            <section className="mt-[80px] flex flex-col lg:flex-row justify-center items-center gap-8 p-12 h-full text-white">
+                <div className="text-primary-dark space-y-6">
+                    <h1 className="text-6xl font-bold font-beauRivage mb-6">Hua Wang</h1>
+                    <h2 className="text-3xl font-semibold mb-6 font-mono">Web Developer / Data Engineer</h2>
+                    <p className="mt-4 leading-normal text-wrap text-lg font-poppins">
                         I am a skilled and reliable software engineer with over 4 years of experience specializing in
-                        full stack development. My expertise includes building scalable APIs with several languages,
-                        integrating with cloud services, and designing CI/CD pipelines. I am passionate about solving
-                        complex problems, optimizing performance, and collaborating across teams to deliver high-quality
-                        software solutions.
+                        full stack development. My expertise includes building scalable APIs, integrating with cloud
+                        services, and designing CI/CD pipelines.
                     </p>
-                    <div className="download-social">
-                        <a href="#" className="btn">
+                    <div className="mt-12 flex items-center">
+                        <a
+                            href="#"
+                            className="btn bg-primary text-light px-4 py-2 rounded shadow-lg hover:bg-secondary"
+                        >
                             Download CV
                         </a>
-                        <div className="social-icons">
-                            <a href="#">
+                        <div className="social-icons ml-6 flex space-x-4">
+                            <a
+                                href="#"
+                                className="w-10 h-10 bg-primary inline-flex items-center justify-center p-2 border-1 border-primary rounded-full text-[20px] text-white no-underline transition duration-500 hover:bg-secondary hover:text-bg hover:shadow-lg"
+                            >
                                 <i className="ri-github-fill"></i>
                             </a>
-                            <a href="#">
+                            <a
+                                href="#"
+                                className="w-10 h-10 bg-primary inline-flex items-center justify-center p-2 border-1 border-primary rounded-full text-[20px]  text-white no-underline transition duration-500 hover:bg-secondary hover:text-bg hover:shadow-lg"
+                            >
                                 <i className="ri-linkedin-fill"></i>
                             </a>
-                            <a href="#">
+                            <a
+                                href="#"
+                                className="w-10 h-10 bg-primary inline-flex items-center justify-center p-2 border-1 border-primary rounded-full text-[20px]  text-white no-underline transition duration-500 hover:bg-secondary hover:text-bg hover:shadow-lg"
+                            >
                                 <i className="ri-google-fill"></i>
                             </a>
                         </div>
                     </div>
                 </div>
-                <div className="home-img">
-                    <div className="img-box">
-                        {/* <ParticleImageEffect png={pngBase64} /> */}
+                <div>
+                    <div className="relative w-[25vw] h-[25vw] border-4 border-primary-dark rounded-full shadow-md overflow-hidden bg-light animate-hueRotate">
                         <EffectPixelDistortion
                             src={hua}
                             style={{ width: "100%", height: "100%", cursor: "all-scroll" }}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,68 @@
 export default {
   content: [
     './index.html',
-    './src/**/*.{js,jsx}', 
+    './src/**/*.{js,jsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        audiowide: ['"Audiowide"', 'cursive'],
+        beauRivage: ['"Beau Rivage"', 'serif'],
+        figtree: ['"Figtree"', 'sans-serif'],
+        poppins: ['"Poppins"', 'sans-serif'],
+      },
+      colors: {
+        primaryDark: '#1d2012', // 深绿色
+        primary: '#afcc8f', // 浅绿色
+        secondary: '#7ca65c', // 中等绿色
+        accent: '#5d7d4b', // 次要绿色
+        muted: '#768e90', // 蓝绿色
+        bg: '#f4f7f4', // 背景灰绿色
+        textDark: '#1a1a1a', // 深色文本
+        textLight: '#ffffff', // 浅色文本
+      },
+      animation: {
+        hueRotate: "hueRotate 5s linear infinite",
+        zoomIn: "zoomIn 1s ease forwards",
+        slideIn: "slideIn 1s ease forwards",
+      },
+      keyframes: {
+        hueRotate: {
+          "0%": {
+            boxShadow: "0 0 20px var(--color-primary-dark)",
+            borderColor: "var(--color-primary-dark)",
+          },
+          "20%": {
+            boxShadow: "0 0 40px var(--color-primary)",
+            borderColor: "var(--color-primary)",
+          },
+          "50%": {
+            boxShadow: "0 0 60px var(--color-secondary)",
+            borderColor: "var(--color-secondary)",
+          },
+          "80%": {
+            boxShadow: "0 0 40px var(--color-accent)",
+            borderColor: "var(--color-accent)",
+          },
+          "100%": {
+            boxShadow: "0 0 20px var(--color-muted)",
+            borderColor: "var(--color-muted)",
+          },
+        },
+        zoomIn: {
+          "0%": { transform: "scale(0)", opacity: "0" },
+          "100%": { transform: "scale(1)", opacity: "1" },
+        },
+        slideIn: {
+          "0%": { transform: "translateY(30px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      borderRadius: {
+        DEFAULT: "0.25rem", // 默认边框半径
+        full: "50%", // 圆形边框
+      },
+    },
   },
   plugins: [],
 }
